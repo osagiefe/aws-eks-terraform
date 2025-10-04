57750632ce9b5e18221573d47ab2f70f002ca0e1
osagiefe <felix.osagie@hotmail.com>
2025-10-04 12:44:59 -0400

    file update

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@
 
 variable "public_subnet1" {
   description = "CIDR Block for Public Subnet 1"
-  default     = "subnet-98dba5e2"
+  default     = "subnet-029e8eb6a6703df97"
 }
 
 variable "public_subnet2" {
   description = "CIDR Block for PublicSubnet 1"
-  default     = "subnet-4356da0f"
+  default     = "subnet-0f67c047fab94ceb7"
 }
 
 variable "cluster_name" {
